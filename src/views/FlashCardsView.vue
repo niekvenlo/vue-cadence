6f34ae6f13a9fd52747f61f181f4f4e1dd759326
Nick Beaujean <niekvenlo@gmail.com>
2025-06-10 19:15:22 +0200

    no hollebolle

--- src/views/FlashCardsView.vue
+++ src/views/FlashCardsView.vue
@@ -30,11 +30,13 @@ const handlePaste = (event: ClipboardEvent) => {
   const pastedText = event.clipboardData?.getData('text') // || window.clipboardData.getData("text");
   if (pastedText === undefined) {
     error.value = 'Could not read from clipboard'
+    setTimeout(() => (error.value = ''), 2000)
     return
   }
   const arrays = pastedText.split(/\n/).map((line) => line.split(/\t/))
   if (arrays.length < 2) {
     error.value = 'Could not parse pasted data into cards'
+    setTimeout(() => (error.value = ''), 2000)
     return
   }
   error.value = ''
@@ -61,62 +63,12 @@ const handleColumnSelection = (idx: number) => {
   width: 100%;
   height: 100%;
   --color-angle: 50deg;
-  #paste-here {
-    flex-grow: 1;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    position: absolute;
 
-    textarea {
-      background-position: center;
-      background-image: url('/flash-instructions-1.png');
-      background-size: contain;
-      background-repeat: no-repeat;
-      flex-grow: 1;
-      width: 100%;
-      height: 100%;
-      &:focus {
-        background-size: cover;
-        background-image: radial-gradient(
-            circle at center,
-            transparent,
-            transparent 40%,
-            white 50%,
-            white 100%
-          ),
-          url('/flash-instructions-2.png');
-      }
-    }
-
-    &::after {
-      position: absolute;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      content: 'Ctrl-V';
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      font-family: sans-serif;
-      font-size: 20vmin;
-      color: hsl(269, 100%, 25%);
-      text-shadow: white 3px 3px 3px;
-      pointer-events: none;
-    }
-
-    &:focus-within::after {
-      content: 'Ctrl-P';
-      color: hsl(269, 100%, 75%);
-      text-shadow: black 3px 3px 3px;
-    }
-  }
   #data-entry-wrapper {
     min-height: 100vh;
     .error {
       padding: 1em;
-      background: black;
+      background: darkred;
       color: white;
     }
     .top-bar {
@@ -138,8 +90,9 @@ const handleColumnSelection = (idx: number) => {
     table {
       width: 100%;
       border-collapse: collapse;
+      th,
       td {
-        text-align: center;
+        padding-inline: 2vw;
         color: hsl(231, 23%, 22%);
         &:nth-of-type(2n) {
           color: hsl(0, 23%, 22%);
@@ -148,18 +101,66 @@ const handleColumnSelection = (idx: number) => {
       }
       thead,
       tr:nth-of-type(2n) {
+        text-align: left;
         background-color: hsl(60, 86.2%, 82.9%);
+        button {
+          width: max-content;
+        }
       }
     }
   }
   h2 {
     text-align: right;
+    padding-right: 1em;
   }
   .explanation {
-    padding-top: 2em;
+    padding: 8em 2em;
     font-size: 1.2em;
     max-width: 70ch;
     margin: auto;
+    div {
+      position: relative;
+      &:focus-within::before {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        content: 'Paste here';
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        font-family: sans-serif;
+        font-size: 10vmin;
+        color: hsl(269, 100%, 25%);
+        text-shadow: white 0px 0px 3px;
+        pointer-events: none;
+      }
+    }
+    textarea {
+      color: transparent;
+      background-position: center;
+      background-size: contain;
+      background-repeat: no-repeat;
+      flex-grow: 1;
+      width: 100%;
+      height: 100%;
+      aspect-ratio: 2/1;
+      &:focus {
+        background-size: cover;
+        background-image: radial-gradient(circle at center center, #1a8fe5, #e4e4ed),
+          repeating-radial-gradient(
+            circle at center center,
+            hsl(269, 100%, 95%),
+            hsl(269, 100%, 95%),
+            22px,
+            transparent 34px,
+            transparent 22px
+          );
+        background-blend-mode: multiply;
+        background-color: #e4e4ed;
+      }
+    }
   }
 }
 </style>
@@ -183,48 +184,75 @@ const handleColumnSelection = (idx: number) => {
       ></FlashCards>
     </div>
 
-    <div id="paste-here" v-if="cards === null">
-      <textarea @paste="handlePaste" autocorrect="off" autocapitalize="off" spellcheck="false" />
-    </div>
-    <div id="data-entry-wrapper" v-else>
-      <div v-if="error" class="error">{{ error }}</div>
-      <div class="top-bar" v-if="cards !== null">
+    <div id="data-entry-wrapper">
+      <div class="top-bar">
         <h1>{{ isDimsumView ? 'dim sum' : 'standard' }} style</h1>
         <div>
           <button class="black" @click="isDimsumView = !isDimsumView">
             Switch to {{ isDimsumView ? 'standard flash cards' : 'dim sum' }}
           </button>
-          <button v-if="cards" class="black" @click="reset">Clear current cards</button>
+          <button v-if="cards" class="black" @click="reset">
+            Forget current {{ cards.length }} cards
+          </button>
         </div>
       </div>
-      <div v-if="cards.length">
-        <h2>{{ cards.length }} entries.</h2>
-        <p v-if="promptIdx === null" class="explanation">
-          You need to choose a column to use as your flashcard 'front' side.
-        </p>
-        <p v-else-if="!isOnlyOneColumn" class="explanation">
-          Now that you have selected the 'front', you may also choose which columns to use as your
-          flashcard 'back' side. (Without a back side, you can only use standard flash cards.)
-        </p>
-        <table cellpadding="0">
-          <thead>
-            <th v-for="(column, idx) in cards[0]" :key="column">
-              <button
-                v-if="idx !== promptIdx && !responseIndices.includes(idx)"
-                class="black"
-                @click="() => handleColumnSelection(idx)"
-              >
-                {{ promptIdx === null ? 'Use as front' : 'Use as back' }}
-              </button>
-              <p v-else-if="idx === promptIdx">Front</p>
-              <p v-else>Back</p>
-            </th>
-          </thead>
-          <tr v-for="card in cards" :key="card[0]">
-            <td v-for="column in card" :key="column">{{ column }}</td>
-          </tr>
-        </table>
+      <div class="explanation" v-if="cards === null">
+        <h2>Step 1</h2>
+        <p>You'll need to copy/paste some data from a table or a spreadsheet.</p>
+        <div>
+          <textarea
+            @paste="handlePaste"
+            autocorrect="off"
+            autocapitalize="off"
+            spellcheck="false"
+          />
+        </div>
       </div>
+      <div class="explanation" v-else-if="promptIdx === null">
+        <h2>Step 2</h2>
+        <p>You need to choose a column to use as your flashcard 'front' side.</p>
+        <p v-if="!isDimsumView">
+          (Since you have selected standard style, when you make your choice, the cards will
+          automatically scroll into view.)
+        </p>
+      </div>
+      <div class="explanation" v-else-if="isOnlyOneColumn">
+        <h2>Step 3</h2>
+        <p>The data you've pasted only has one column, so you can only use standard flash cards.</p>
+      </div>
+      <div class="explanation" v-else>
+        <h2>Step 3</h2>
+        <p>You may also choose which columns to use as your flashcard 'back' side.</p>
+        <p>
+          (Dimsum style requires a back side. Without a back side, you can only use standard flash
+          cards.)
+        </p>
+      </div>
+
+      <div v-if="error" class="error">{{ error }}</div>
+
+      <table cellpadding="0" v-if="cards !== null">
+        <thead>
+          <th v-for="(column, idx) in cards[0]" :key="column">
+            <button
+              v-if="idx !== promptIdx && !responseIndices.includes(idx)"
+              class="light"
+              @click="() => handleColumnSelection(idx)"
+            >
+              {{
+                promptIdx === null
+                  ? 'Use this column for the front'
+                  : 'Use this column for the back'
+              }}
+            </button>
+            <p v-else-if="idx === promptIdx">Front</p>
+            <p v-else>Back</p>
+          </th>
+        </thead>
+        <tr v-for="card in cards" :key="card[0]">
+          <td v-for="column in card" :key="column">{{ column }}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
